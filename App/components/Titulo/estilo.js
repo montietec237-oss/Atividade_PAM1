31e90660c49a81dba785d9ec042c18dc8fdf036c
montietec237-oss <montietec.237@gmail.com>
2026-08-14 13:11:44 +0000

    Fim Exemplo1

---
+++ App/components/Titulo/estilo.js
@@ -0,0 +1,17 @@
+
+import { StyleSheet } from 'react-native';
+
+const estilo = StyleSheet.create({
+  box: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  texto:{
+    fontSize: 28,
+    padding: 10,
+    color: '#61dafb'
+  }
+});
+
+export default estilo;
